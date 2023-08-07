383ad193961e626862622401726d32881233b891
Your Name <mikhailmakarov92@gmail.com>
2023-08-08 02:06:32 +0400

    Adobe task04

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -5,8 +5,8 @@ Console.WriteLine("Введите число 1");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число 2");
 int num2 = Convert.ToInt32(Console.ReadLine());
-if (num1 > num2) Console.WriteLine ($"min = {num2} max = {num1}");
-if (num1 < num2) Console.WriteLine ($"min = {num1} max = {num2}");
+if (num1 > num2) Console.WriteLine($"min = {num2} max = {num1}");
+if (num1 < num2) Console.WriteLine($"min = {num1} max = {num2}");
 else
 {
     Console.WriteLine("Числа равны");
