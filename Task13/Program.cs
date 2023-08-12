254eacbded8fae61b079392acf22c36cfd895fea
Your Name <mikhailmakarov92@gmail.com>
2023-08-12 23:35:31 +0400

    Adobe Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 0) num*=-1;
+
+int ThreeDigit(int numb)
+{
+    while (numb > 999)
+    {
+         numb = numb/10;
+    }
+    return numb;
+}
+
+if (num < 100) Console.WriteLine($"У числа {num} -> третьей цифры нет");
+else
+{
+    int threeDigit = ThreeDigit(num)%10;
+    Console.WriteLine($"У числа {num} -> третьея цифра {threeDigit}");
+}
