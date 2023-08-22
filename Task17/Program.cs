f6aacd4dc9afe991bb0c4fdaccd34d24356273bd
Your Name <mikhailmakarov92@gmail.com>
2023-08-22 14:57:16 +0400

    Adobe Task18

--- Task17/Program.cs
+++ Task17/Program.cs
@@ -3,12 +3,12 @@
 // номер четверти плоскости, в которой находится эта
 // точка.
 
-int Quarter(int x, int y)
+int Quarter(int x, int y) // x = 6, y = -7
 {
-    if (x > 0 && y > 0) return 1;
-    if (x < 0 && y > 0) return 2;
-    if (x < 0 && y < 0) return 3;
-    if (x > 0 && y < 0) return 4;
+    if(x > 0 && y > 0) return 1;
+    if(x < 0 && y > 0) return 2;
+    if(x < 0 && y < 0) return 3;
+    if(x > 0 && y < 0) return 4;
     return 0;
 }
 
@@ -19,6 +19,8 @@ Console.Write("Y: ");
 int yCoordinate = Convert.ToInt32(Console.ReadLine());
 
 int quarter = Quarter(xCoordinate, yCoordinate);
-string result = quarter > 0 ? $"Указанные координаты соответсвуют четверти -> {quarter}"
-                            : "Введены некорректные координаты";
+
+string result = quarter > 0 
+                ? $"Указанные координаты соответствуют четверти -> {quarter}" 
+                : "Введены некорректные координаты";
 Console.WriteLine(result);
