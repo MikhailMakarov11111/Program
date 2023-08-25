adac88b905e0a345b85e56b57ee550f3f08c8e5f
Your Name <mikhailmakarov92@gmail.com>
2023-08-25 15:08:14 +0400

    Adobe Task26

---
+++ Task26/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 26: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int Quantity(int num)
+{
+    // if (num < 0) num = num * -1;
+    int count = 0;
+    while (num > 0 || num <0)
+    {
+        num = num / 10;
+        count++;
+    }
+    return count;
+}
+
+Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int quantity = Quantity(number);
+Console.WriteLine($"В числе {number} -> {quantity} знаков");
