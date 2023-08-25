ebd544940e8d9ff3ceb47aefceab43b0a5a5abcd
Your Name <mikhailmakarov92@gmail.com>
2023-08-25 15:29:19 +0400

    Adobe Task28

--- Task26/Program.cs
+++ Task26/Program.cs
@@ -7,8 +7,9 @@
 int Quantity(int num)
 {
     // if (num < 0) num = num * -1;
+    if (num == 0) return 1;
     int count = 0;
-    while (num > 0 || num <0)
+    while (num != 0)
     {
         num = num / 10;
         count++;
