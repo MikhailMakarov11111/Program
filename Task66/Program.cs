938de0cd3af8127aba192bc8f76594c48c95cd21
Your Name <mikhailmakarov92@gmail.com>
2023-09-15 05:59:25 +0400

    Adobe Task66

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. 
+// Выполнить с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите первое натуральное число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе натуральное число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 < 1 || number2 < 1)
+{
+    Console.WriteLine("Ошибка ввода!");
+    return;
+}
+
+int SumNaturalNumbers(int num1, int num2)
+{
+    if (num1 < num2)
+    {
+       return num1 + SumNaturalNumbers(num1 + 1, num2);
+    }
+    else if (num1 > num2)
+    {
+       return num1 + SumNaturalNumbers(num1 - 1, num2);
+    }
+    else return 0;
+}
+
+int sumNaturalNumbers = SumNaturalNumbers(number1, number2) + number2;
+Console.WriteLine(sumNaturalNumbers);
